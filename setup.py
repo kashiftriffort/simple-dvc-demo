1e89565af19d4a9a449a5c6ee602e67cf9ab4e82
Kashif Jilani <kashifjilani@Kashifs-MacBook-Pro.local>
2023-02-24 16:52:11 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="its a wine Q package", 
+    author="Kashif Jilani", 
+    packages=find_packages(),
+    license="MIT"
+)
